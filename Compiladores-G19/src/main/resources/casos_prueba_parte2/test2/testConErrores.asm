da7c977579487380f2cf037a168e256fd6006af1
Santino Lucarini <slucarini@baufest.com>
2024-11-20 17:42:09 -0300

    Version entrega martes

--- Compiladores-G19/src/main/resources/casos_prueba_parte2/test2/testConErrores.asm
+++ Compiladores-G19/src/main/resources/casos_prueba_parte2/test2/testConErrores.asm
@@ -15,10 +15,14 @@ OperacionEnteroNegativo db "El resultado de la operacion no puede ser negativo."
 OverflowSumaDouble db "Se produjo un un overflow en la suma de doubles.", 0 
 error db "Error", 0 
 printMensaje db "Print", 0 
+_c:main db ?
+_total:main db ?
+_b:main db ?
 _1 db 1
-_x:main db ?
-_y:main db ?
-@print1 db "y:main", 0 
+_2 db 2
+_5 db 5
+_a:main db ?
+_10 db 10
 
 .code
 invoke MessageBox, NULL, addr AutoinvocacionFunciones, addr error, MB_OK 
@@ -27,14 +31,24 @@ invoke MessageBox, NULL, addr OperacionEnteroNegativo, addr error, MB_OK
 invoke ExitProcess, 0 
 invoke MessageBox, NULL, addr OverflowSumaDouble, addr error, MB_OK 
 invoke ExitProcess, 0 
+devuelveUno:main:
+MOV EAX, 1
+CMP EAX, 2
+JLE label1
+MOV EAX, 1
+MOV @aux2, EAX
+ret 
+
+MOV EAX, 1
+MOV @aux3, EAX
+ret 
+JMP errorFun
 main:
 MOV EAX ,1
-MOV x:main, EAX
-MOV EAX, 1
-ADD EAX, x:main
-MOV @aux1, EAX
-MOV EAX ,@aux1
-MOV y:main, EAX
-invoke MessageBox, NULL, addr @print1, addr printMensaje, MB_OK
+MOV a:main, EAX
+MOV EAX ,5
+MOV b:main, EAX
+MOV EAX ,10
+MOV c:main, EAX
 invoke ExitProcess, 0 
 end main
--- Compiladores-G19/src/main/resources/casos_prueba_parte2/test3/test.asm
+++ Compiladores-G19/src/main/resources/casos_prueba_parte2/test3/test.asm
@@ -15,10 +15,14 @@ OperacionEnteroNegativo db "El resultado de la operacion no puede ser negativo."
 OverflowSumaDouble db "Se produjo un un overflow en la suma de doubles.", 0 
 error db "Error", 0 
 printMensaje db "Print", 0 
+_y:main db ?
+_z:main db ?
 _1 db 1
 _x:main db ?
-_y:main db ?
-@print1 db "y:main", 0 
+_2 db 2
+_3 db 3
+_2.0 dq 2.0
+@print1 db "z:main:f3", 0 
 
 .code
 invoke MessageBox, NULL, addr AutoinvocacionFunciones, addr error, MB_OK 
@@ -27,14 +31,41 @@ invoke MessageBox, NULL, addr OperacionEnteroNegativo, addr error, MB_OK
 invoke ExitProcess, 0 
 invoke MessageBox, NULL, addr OverflowSumaDouble, addr error, MB_OK 
 invoke ExitProcess, 0 
-main:
-MOV EAX ,1
-MOV x:main, EAX
-MOV EAX, 1
-ADD EAX, x:main
-MOV @aux1, EAX
-MOV EAX ,@aux1
-MOV y:main, EAX
+f3:main:
+MOV EAX, x:main
+CMP EAX, 3
+JLE label1
 invoke MessageBox, NULL, addr @print1, addr printMensaje, MB_OK
+JMP label2
+label1:
+label2:
+
+MOV EAX, 2
+ADD EAX, 2
+MOV @aux3, EAX
+MOV EAX, @aux3
+MOV @aux2, EAX
+ret 
+JMP errorFun
+main:
+MOV EAX ,3
+MOV x:main, EAX
+MOV EAX ,2
+MOV y:main, EAX
+MOV EAX, x:main
+ADD EAX, y:main
+MOV @aux4, EAX
+MOV EAX ,@aux4
+MOV z:main, EAX
+MOV EAX, y:main
+MOV EAX, y:main
+call f3:main
+MOV EAX, y:main
+MOV EAX, y:main
+call f3:main
+FLD 2.0
+FST t1[1]
+MOV EAX ,t1[1]
+MOV x:main, EAX
 invoke ExitProcess, 0 
 end main
