289b5596f72b20226d5d6ee46fa7eb2d6195b2a3
orsingheraxel <orsingheraxel@medialab.com.ar>
2024-11-22 20:20:16 -0300

    Agrega test

--- Compiladores-G19/src/main/resources/casos_de_prueba/test8/test.asm
+++ Compiladores-G19/src/main/resources/casos_de_prueba/test8/test.asm
@@ -15,20 +15,17 @@ OperacionEnteroNegativo db "El resultado de la operacion no puede ser negativo."
 OverflowSumaDouble db "Se produjo un un overflow en la suma de doubles.", 0 
 error db "Error", 0 
 printMensaje db "Print", 0 
-@aux4 dd ?
 _1 dd 1
 _2 dd 2
-_y@main dd ?
-_3 dd 3
-_5 dd 5
-_t1@main dd ?,?,?
-_z@main dd ?
+@aux5 dd ?
+@aux4 dd ?
+_i@main dd ?
+_j@main dd ?
+_m@main dd ?
+_6 dd 6
 @aux3 dd ?
 @aux2 dd ?
 @aux1 dd ?
-_x@main dd ?
-@print1 db [_i@main@f3], 0 
-@print2 db "i es igual a 1", 0 
 
 .code
 invoke MessageBox, NULL, addr AutoinvocacionFunciones, addr error, MB_OK 
@@ -37,38 +34,40 @@ invoke MessageBox, NULL, addr OperacionEnteroNegativo, addr error, MB_OK
 invoke ExitProcess, 0 
 invoke MessageBox, NULL, addr OverflowSumaDouble, addr error, MB_OK 
 invoke ExitProcess, 0 
-f3@main:
-PUSH EBP
-MOV EBP, ESP
-MOV EAX, [EBP + 8]
-MOV EAX, x@main
-CMP EAX, 3
-JLE  label1
-invoke MessageBox, NULL, addr @print1, addr @print1, MB_OK
+start:
+MOV EAX, i@main
+CMP EAX, 6
+JL  label1
+MOV EAX, j@main
+CMP EAX, 2
+JL  label1
+MOV EAX, _2
+MOV _i@main, EAX
 JMP label2
 label1:
 MOV EAX, _1
-MOV _i@main@f3, EAX
-invoke MessageBox, NULL, addr @print2, addr @print2, MB_OK
-label2:
-MOV EAX, _5
-MOV _x@main, EAX
-MOV EAX, _i@main@f3
-ret 
-JMP errorFun
-start:
-MOV EAX, _3
-MOV _x@main, EAX
+MOV _i@main, EAX
+MOV EAX, _1
+MOV _m@main, EAX
+MOV EAX, m@main
+label4:
+CMP EAX, 2
+JL  label3
+JMP label6:
+label3:
+MOV EAX, m@main
+CMP EAX, 1
+JG  label5
+JMP label6:
+label5:
 MOV EAX, _2
-MOV _y@main, EAX
-MOV EAX, _x@main
-ADD EAX, _y@main
-MOV _z@main, EAX
-MOV EAX, _y@main
-PUSH EAX
-CALL f3@main
-ADD ESP, 4
-MOV EAX, @aux4
-MOV [_t1@main + 0], EAX
+MOV _i@main, EAX
+MOV EAX, _m@main
+ADD EAX, _1
+MOV @aux3, EAX
+MOV _m@main, @aux3
+JMP label4
+label6:
+label2:
 invoke ExitProcess, 0 
 end start
